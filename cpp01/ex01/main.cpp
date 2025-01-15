cbd7c8be367ab2c7e870229cddced63e7454d728
romain pepi <rpepi@student.s19.be>
2025-01-15 12:09:45 +0100

    delete

--- cpp01/ex01/main.cpp
+++ cpp01/ex01/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pepi <pepi@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: rpepi <rpepi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 13:30:07 by pepi              #+#    #+#             */
-/*   Updated: 2024/11/14 13:37:36 by pepi             ###   ########.fr       */
+/*   Updated: 2025/01/15 12:09:12 by rpepi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int main(void)
 {
-    int N = 1;
+    int N = 5;
     Zombie* horde = zombieHorde(N, "HordeZombie");
 
     if (horde) 
@@ -23,7 +23,8 @@ int main(void)
         {
                horde[i].announce();
         }
-         delete horde;
+        delete[] horde;
+
     }
     return 0;
 }
